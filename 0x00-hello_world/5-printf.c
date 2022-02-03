5343efdfb97db7071155586c008438b8cd4d4928
Luis Manrique <luismanrique158158@gmail.com>
2022-02-03 22:06:33 +0000

    Fix betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
+ * main --> Entry point
  *
+ * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	printf ("%s\n","with proper grammar, but the outcome is a piece of art,");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
