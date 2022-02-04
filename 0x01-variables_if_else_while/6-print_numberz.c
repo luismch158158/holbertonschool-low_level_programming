bc22953ced7cf7ed1839f07218e915602534e417
Luis Manrique <luismanrique158158@gmail.com>
2022-02-04 22:12:15 +0000

    Adding program write numbers with ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Return value "n" and "i" (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
