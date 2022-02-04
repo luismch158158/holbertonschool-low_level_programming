a8b9b089e4102f30dfc0fbedf5a33212fc436269
Luis Manrique <luismanrique158158@gmail.com>
2022-02-04 22:17:36 +0000

    Adding program lowercase alphabet reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Return value "n" and "i" (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
