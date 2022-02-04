71a57bcd368ded8f07b3fbe119cff6cc69042b9e
Luis Manrique <luismanrique158158@gmail.com>
2022-02-04 22:03:03 +0000

    Adding program with statement for print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Return value "n" and "i" (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d",i);
+	}
+	putchar('\n');
+	return (0);
+}
