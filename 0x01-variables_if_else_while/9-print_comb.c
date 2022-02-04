abacfdf4594db6ea973f1a6d6be0f41893edcbbb
Luis Manrique <luismanrique158158@gmail.com>
2022-02-04 23:09:25 +0000

    Adding program that print all combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Return value "n" and "i" (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+		if (i <= '8')
+		{
+			putchar(32);
+			putchar(44);
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
