ed0ce782ceb9249dc76ce4f13688c3a687560578
Luis Manrique <luismanrique158158@gmail.com>
2022-02-10 21:39:12 +0000

    Adding program that draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+
+}
