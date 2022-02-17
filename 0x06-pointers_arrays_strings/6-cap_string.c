b046d8f3207fe241a9d428436a3f42c4f6f3a4af
Luis Manrique <luismanrique158158@gmail.com>
2022-02-17 15:54:53 +0000

    Fix null byte add

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -36,8 +36,6 @@ char *cap_string(char *str)
 
 			if (k == 0)
 			{
-				if (str[i] == '\t')
-					str[i] = ' ';
 				if ((str[i + 1]) >= 97 && ((str[i + 1]) <= 122))
 					str[i + 1] = str[i + 1] - 32;
 			}
