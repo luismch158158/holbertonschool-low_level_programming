aaf7a80a5eabdbac8b271b932c25e5fd68c0388d
Luis Manrique <luismanrique158158@gmail.com>
2022-02-17 15:47:07 +0000

    Fix tabulation error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -36,6 +36,8 @@ char *cap_string(char *str)
 
 			if (k == 0)
 			{
+				if (str[i] == '\t')
+					str[i] = ' ';
 				if ((str[i + 1]) >= 97 && ((str[i + 1]) <= 122))
 					str[i + 1] = str[i + 1] - 32;
 			}
