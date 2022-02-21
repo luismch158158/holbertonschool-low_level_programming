262dfbaf32fd0faab13d68fd496f4c5afd9fb851
Luis Manrique <luismanrique158158@gmail.com>
2022-02-21 19:49:20 +0000

    Adding stdlib.h

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _strchr - funcion that locates a character in a string
@@ -18,5 +19,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (0);
+	return (NULL);
 }
