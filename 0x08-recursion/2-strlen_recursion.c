7fff2d9d8fb6a2d8102dfe615fc57b722cc5b684
Luis Manrique <luismanrique158158@gmail.com>
2022-02-23 21:53:12 +0000

    Fix betty style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * set_string - funcion that returns the length of a string
+ * _strlen_recursion - funcion that returns the length of a string
  * @s: pointer to string
  * Return: length of a string
  */
 
 int _strlen_recursion(char *s)
-{	
+{
 	if (*s != '\0')
 	{
 		return (1 + _strlen_recursion(s + 1));
