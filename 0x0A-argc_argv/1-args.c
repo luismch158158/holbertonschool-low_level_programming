079fea558b632e35c77a93d93478d650606f1491
Luis Manrique <luismanrique158158@gmail.com>
2022-02-25 15:56:18 +0000

    Adding program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: Count the number of things that I pass
+ * on the command line when I run this program
+ * @argv: Array that contains the string values
+ * of things that were entered on the command line
+ * when I run this program
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc __attribute__((unused)),
+		char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
