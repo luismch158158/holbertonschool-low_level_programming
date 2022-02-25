69716246055af180b5d0717b98bcacaf8c650f6a
Luis Manrique <luismanrique158158@gmail.com>
2022-02-25 16:05:19 +0000

    Adding program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: Count the number of things that I pass
+ * on the command line when I run this program
+ * @argv: Array that contains the string values
+ * of things that were entered on the command line
+ * when I run this program
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc __attribute__((unused)),
+		char **argv __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
