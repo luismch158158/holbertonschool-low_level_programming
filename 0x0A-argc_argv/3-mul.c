09948ab0acba19dc3e65724270d42a1337cdac33
Luis Manrique <luismanrique158158@gmail.com>
2022-02-25 16:15:11 +0000

    Adding program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: Count the number of things that I pass
+ * on the command line when I run this program
+ * @argv: Array that contains the string values
+ * of things that were entered on the command line
+ * when I run this program
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc __attribute__((unused)),
+		char **argv __attribute__((unused)))
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	return (0);
+}
