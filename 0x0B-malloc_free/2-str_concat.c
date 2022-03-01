16d069da757e6de13a594c48ef40b6163eabad79
Luis Manrique <luismanrique158158@gmail.com>
2022-03-01 19:58:01 +0000

    Fix NULL byte to be treated as empty space

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,16 @@ char *str_concat(char *s1, char *s2)
 	unsigned int j;
 	unsigned int k;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		continue;
