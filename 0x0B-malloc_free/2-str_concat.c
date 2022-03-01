d369a8c4fe8f147d432bf0df383ebac2708af6c0
Luis Manrique <luismanrique158158@gmail.com>
2022-03-01 20:00:50 +0000

    Fix betty style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,7 +8,6 @@
  *
  * Return: pointer to two strings concatenates
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
@@ -17,46 +16,30 @@ char *str_concat(char *s1, char *s2)
 	unsigned int k;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	
+
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		continue;
-	}
 
 	for (j = 0; s2[j] != '\0'; j++)
-	{
 		continue;
-	}
 
 	j = j + 1;
 
 	s = malloc((i + j) * sizeof(char));
 
 	if (s == NULL)
-	{
 		return (0);
-	}
 
 	for (k = 0; k < (i + j) ; k++)
 	{
 		if (k < i)
-		{
 			s[k] = s1[k];
-		}
 		if (k >= i)
-		{
 			s[k] = s2[k - i];
-		}
 	}
-
 	return (s);
-
 }
