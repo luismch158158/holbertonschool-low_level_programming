f0520350ddfc0cc94aea9de10434396ac39104e3
Luis Manrique <luismanrique158158@gmail.com>
2022-03-08 01:25:43 +0000

    Update dog.h for task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - Structure to save Poppy information
  * @name: name of dog
@@ -14,5 +17,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog  dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /*DOG_H*/
