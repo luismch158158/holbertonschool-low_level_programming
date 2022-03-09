30727f0f51f8bc5669adb2c2f0e7bb02efd330c7
Luis Manrique <luismanrique158158@gmail.com>
2022-03-09 23:58:44 +0000

    Fix version 2 of code

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,12 +20,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 5; i++)
+	while (ops[i].op)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
+		i++;
 	}
 
 	return (NULL);
