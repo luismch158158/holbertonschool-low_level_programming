3bd3f90db017dbdc4c89e43a45c0efbb9f168fa9
Luis Manrique <luismanrique158158@gmail.com>
2022-03-10 16:22:02 +0000

    Adding function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - Function that sum of all it parameters
+ * @separator: string to be printed between numbers
+ * @n: numbers of parameters to passing
+ * @...: parameters to passing in compilation
+ */
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list number;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	if (n == 0)
+		return;
+
+	va_start(number, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d\n", va_arg(number, int));
+			break;
+		}
+
+		printf("%d", va_arg(number, int));
+		printf("%s", separator);
+	}
+	va_end(number);
+}
