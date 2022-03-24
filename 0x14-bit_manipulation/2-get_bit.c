c8c0093e65d0d006bd617bdca83cc6ca45fa6ff0
Luis Manrique <luismanrique158158@gmail.com>
2022-03-24 19:27:42 +0000

    Fix validation of index is higher than 63

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -19,7 +19,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index == 0)
 		return ((n & 1));
 
-	if (n < 2 && index > 0)
+	if (index > 63)
 		return (-1);
 
 	if (n > 1 || index == 0)
