692e56d2a7f4fa9376c064305dfe5f4a9dcaff67
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-27 09:46:32 -0500

    Fix get value in the node in the linked list with collision

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	char *value = NULL;
 	unsigned long int index = 0;
-	hash_node_t *node = NULL;
+	hash_node_t *node = NULL, *tmp = NULL;
 
 	if (!ht || !(ht->array) || !key)
 		return (value);
@@ -25,7 +25,20 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (!node)
 		return (value);
 
-	value = node->value;
+	if (strcmp(node->key, key) == 0)
+	{
+		value = node->value;
+		return (value);
+	}
+
+	for (tmp = node; tmp; tmp = tmp->next)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			value = tmp->value;
+			return (value);
+		}
+	}
 
 	return (value);
 }
