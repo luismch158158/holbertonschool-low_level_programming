f8fb36d9b19e1be1b9d8667980fcd1edb25bf956
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-31 10:51:24 -0500

    fix betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,10 @@
 #include "search_algos.h"
 
 /**
- * linear_search - function that searches for a value
- * in an array of integers using the Linear search algorithm
- *
+ * linear_search - function that searches for a value linear search
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
- *
  * Return: return the first index where value is located or -1 if not exist
 */
 
