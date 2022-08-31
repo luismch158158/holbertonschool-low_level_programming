dd357793598ceaabad9af7458ff54ff66b01c136
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-08-31 10:49:17 -0500

    change description of return

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,23 +8,21 @@
  * @size: number of elements in array
  * @value: value to search for
  *
- * Return: return the first index where value is located
- * if value is not present in array or if array is NULL,
- * the function return -1
+ * Return: return the first index where value is located or -1 if not exist
 */
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (array == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
 	}
 	return (-1);
 }
